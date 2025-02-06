a9166bd3052d994eb8d8d16114db0fe5567838fa
lutvi alviansyah <134485224+LutviALvi@users.noreply.github.com>
2025-02-06 21:34:58 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,61 @@ function tup() {
 		tutup.style.display = 'none';
 	}
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+	let elements = document.querySelectorAll('.isi h1');
+
+	let observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add('show');
+				}
+			});
+		},
+		{ threshold: 0 }
+	);
+
+	elements.forEach((el) => observer.observe(el));
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+	let elements = document.querySelectorAll('.isi p');
+
+	let observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add('show');
+				}
+			});
+		},
+		{ threshold: 1 }
+	);
+
+	elements.forEach((el) => observer.observe(el));
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+	let elements = document.querySelectorAll('.carosel');
+
+	let observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add('show');
+				}
+			});
+		},
+		{ threshold: 1 }
+	);
+
+	elements.forEach((el) => observer.observe(el));
+});
+
+// paralax
+document.addEventListener('scroll', function () {
+	let banner = document.querySelector('.banner');
+	let scrollPos = window.scrollY;
+	banner.style.backgroundPositionY = `${-10 + scrollPos * 0.5}px`;
+});
